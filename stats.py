01f7371c59957d6f46662381d66118803bce3044
Lucca Gontijo Assis <38539362+Luccagg@users.noreply.github.com>
2026-01-13 16:09:17 -0300

    Add files via upload

--- stats.py
+++ stats.py
@@ -11,7 +11,6 @@ def median(v):
 	n = len(v)
 	sorted_v = sorted(v)
 	midpoint = n//2
-
 	if n % 2 == 1:
 		return sorted_v[midpoint]
 	else:
@@ -28,7 +27,6 @@ def mode(x):
 	max_count = max(counts.values())
 	return [x_i for x_i, count in counts.items()
 				if count == max_count]
-
 
 # Dispersion #
 
@@ -51,10 +49,30 @@ def standard_deviation(x):
 	return math.sqrt(variance(x))
 
 
+def interquartile_range(x):
+	return quantile(x, 0.75) - quantile(x, 0.25)
+
+
+def covariance(x, y):
+	n = len(x)
+	return linear_algebra.dot_product(de_mean(x), de_mean(y))/(n-1)
+
+
+def correlaton(x, y):
+	stdev_x = standard_deviation(x)
+	stdev_y = standard_deviation(y)
+	if stdev_x > 0 and stdev_y > 0:
+		return covariance(x,y) / stdev_x / stdev_y
+	else:
+		return 0
+
 
 if __name__ == '__main__':
 	num_friends = [1, 2, 3, 4, 5, 5]
+	daily_minutes = [3, 4, 5, 6, 10, 9]
 	print(mode(num_friends))
 	print(variance(num_friends))
 	print(standard_deviation(num_friends))
-
+	print(interquartile_range(num_friends))
+	print(covariance(num_friends, daily_minutes))
+	print(correlaton(num_friends, daily_minutes))
